bd284d416e0563b474587d53722e58ce3f3480b4
Moin Uddin <moinku07@gmail.com>
2020-07-30 21:07:37 +0800

    .gitignore fix

--- Pods/Target Support Files/Pods-LandmarkRemark-LandmarkRemarkAcceptanceTests/Pods-LandmarkRemark-LandmarkRemarkAcceptanceTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LandmarkRemark_LandmarkRemarkAcceptanceTests : NSObject
-@end
-@implementation PodsDummy_Pods_LandmarkRemark_LandmarkRemarkAcceptanceTests
-@end
