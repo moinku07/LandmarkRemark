ba22d8c24a213e0da6f5e4d334e8131324edf8d3
S M Moin Uddin <moin.uddin@stjohnambulance.com.au>
2020-07-31 08:38:08 +0800

    Updated AppDelegate to add FirebaseCore

--- LandmarkRemark/AppDelegate.swift
+++ LandmarkRemark/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         FirebaseApp.configure()
         
         // reset the session if it's login UI test
